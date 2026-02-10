47dbca2b0069eebaf8da42d1900830d9c51962aa
Anna Gladush <ann.gladush@gmail.com>
2026-02-10 16:30:09 +0200

    Finished

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,95 @@
 module Enumerable
-  # Your code goes here
+  # each_with_index
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    idx = -1
+    self.my_each do |elem|
+      idx += 1
+      yield(elem, idx)
+    end
+  end
+
+  # select
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    result = []
+    self.my_each do |elem|
+      result << elem if yield(elem) == true
+    end
+    result
+  end
+
+  # all?
+  def my_all?
+    return to_enum(:my_all?) unless block_given?
+
+    result = self.my_select do |elem|
+      yield(elem)
+    end
+    result == self
+  end
+
+  # any?
+  def my_any?
+    return to_enum(:my_any?) unless block_given?
+
+    result = self.my_select do |elem|
+      yield(elem)
+    end
+    result.length >= 1
+  end
+
+  # none?
+  def my_none?
+    return to_enum(:my_none?) unless block_given?
+
+    result = self.my_select do |elem|
+      yield(elem)
+    end
+    result.length < 1
+  end
+
+  # count
+  def my_count
+    return self.size unless block_given?
+
+    result = self.my_select do |elem|
+      yield(elem)
+    end
+    result.length
+  end
+
+  # map
+  def my_map
+    return to_enum(:my_map) unless block_given?
+
+    result = []
+    self.my_each do |elem|
+      result << yield(elem)
+    end
+    result
+  end
+
+  # inject
+  def my_inject(initial_value)
+    return to_enum(:my_inject) unless block_given?
+
+    self.my_each do |elem|
+      initial_value = yield(initial_value, elem)
+    end
+    initial_value
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
   # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    for item in self
+      yield(item)
+    end
+  end
 end
